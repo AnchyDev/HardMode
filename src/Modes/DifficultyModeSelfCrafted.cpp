eaa5af7b71adf838394646ec590c95261645b0ef
AnchyDev <lewienato@gmail.com>
2023-03-31 09:06:40 +1100

    Fix warning for unused parameter and added config check for SelfCrafted.

--- src/Modes/DifficultyModeSelfCrafted.cpp
+++ src/Modes/DifficultyModeSelfCrafted.cpp
@@ -1,6 +1,13 @@
 #include "DifficultyModeSelfCrafted.h"
 
-bool DifficultyModeSelfCrafted::CanSendAuctionHello(WorldSession const* session, ObjectGuid guid, Creature* creature)
+#include "Config.h"
+
+bool DifficultyModeSelfCrafted::CanSendAuctionHello(WorldSession const* /*session*/, ObjectGuid /*guid*/, Creature* /*creature*/)
 {
+    if (!sConfigMgr->GetOption<bool>("HardMode.EnableSelfCrafted", false))
+    {
+        return true;
+    }
+
     return false;
 }
