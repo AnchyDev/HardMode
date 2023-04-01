1c7b027e895b2772fe8ee422d4d98fd3af580e6e
AnchyDev <lewienato@gmail.com>
2023-04-01 11:57:54 +1100

    Add item id as primary key for exclusion table.

--- data/sql/db-world/base/hardmode_world_2023_03_31_01.sql
+++ data/sql/db-world/base/hardmode_world_2023_03_31_01.sql
@@ -8,11 +8,14 @@ CREATE TABLE IF NOT EXISTS `hardmode_rewards` (
   PRIMARY KEY (`mode`,`reward_type`,`reward_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+
 DROP TABLE IF EXISTS `hardmode_selfcraft_exclude`;
 CREATE TABLE IF NOT EXISTS `hardmode_selfcraft_exclude` (
-  `item_id` int(11) DEFAULT NULL,
-  `comment` varchar(50) DEFAULT NULL
+  `item_id` int(11) NOT NULL,
+  `comment` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`item_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 
 DELETE FROM `gameobject_template` WHERE `entry`=441101;
 INSERT INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `size`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `Data6`, `Data7`, `Data8`, `Data9`, `Data10`, `Data11`, `Data12`, `Data13`, `Data14`, `Data15`, `Data16`, `Data17`, `Data18`, `Data19`, `Data20`, `Data21`, `Data22`, `Data23`, `AIName`, `ScriptName`, `VerifiedBuild`) VALUES (441101, 2, 6925, 'Shrine of Hard Mode', '', '', '', 1.2, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 'HardModeGameObjectScript', 0);
