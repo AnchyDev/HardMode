9b8ffc6a7e475e4f57eba29aa67760e3f2e6576b
AnchyDev <lewienato@gmail.com>
2023-04-01 11:10:26 +1100

    Added primary key for hardmode rewards table.

--- data/sql/db-world/base/hardmode_world_2023_03_31_01.sql
+++ data/sql/db-world/base/hardmode_world_2023_03_31_01.sql
@@ -1,10 +1,11 @@
 DROP TABLE IF EXISTS `hardmode_rewards`;
 CREATE TABLE IF NOT EXISTS `hardmode_rewards` (
-  `mode` int(11) DEFAULT NULL,
-  `reward_type` int(11) DEFAULT NULL,
-  `reward_id` int(11) DEFAULT NULL,
+  `mode` int(11) NOT NULL,
+  `reward_type` int(11) NOT NULL,
+  `reward_id` int(11) NOT NULL,
   `reward_count` int(11) DEFAULT NULL,
-  `comment` varchar(50) DEFAULT NULL
+  `comment` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`mode`,`reward_type`,`reward_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 DROP TABLE IF EXISTS `hardmode_selfcraft_exclude`;
