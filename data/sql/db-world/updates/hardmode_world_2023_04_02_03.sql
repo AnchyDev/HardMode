0a343c29d6a71fda6098e60e1a22e0a1133cb53c
AnchyDev <lewienato@gmail.com>
2023-04-02 20:55:29 +1000

    Added shrine locations to all start zones.

---
+++ data/sql/db-world/updates/hardmode_world_2023_04_02_03.sql
@@ -0,0 +1,21 @@
+-- Orc starting zone campfire, Shrine sits in its position.
+DELETE FROM `gameobject` WHERE `guid`=14;
+
+DELETE FROM `gameobject` WHERE `guid`=5530697;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES (5530697, 441101, 0, 0, 0, 1, 1, -8922.74, -145.884, 81.2733, 2.71394, -0, -0, -0.977226, -0.212199, 300, 0, 1, '', NULL);
+DELETE FROM `gameobject` WHERE `guid`=5530698;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES (5530698, 441101, 0, 0, 0, 1, 1, -6213.57, 329.541, 383.707, 3.03243, -0, -0, -0.998511, -0.0545558, 300, 0, 1, '', NULL);
+DELETE FROM `gameobject` WHERE `guid`=5530699;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES (5530699, 441101, 1, 0, 0, 1, 1, 10326.4, 821.202, 1326.45, 2.42121, -0, -0, -0.935829, -0.352454, 300, 0, 1, '', NULL);
+DELETE FROM `gameobject` WHERE `guid`=5530700;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES (5530700, 441101, 530, 0, 0, 1, 1, -3974.18, -13927.5, 100.722, 0.176695, -0, -0, -0.0882326, -0.9961, 300, 0, 1, '', NULL);
+DELETE FROM `gameobject` WHERE `guid`=5530702;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES (5530702, 441101, 1, 0, 0, 1, 1, -602.815, -4250.92, 38.9564, 3.34502, -0, -0, -0.994832, 0.101536, 300, 0, 1, '', NULL);
+DELETE FROM `gameobject` WHERE `guid`=5530703;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES (5530703, 441101, 0, 0, 0, 1, 1, 1656.79, 1673.79, 120.719, 0.22419, -0, -0, -0.11186, -0.993724, 300, 0, 1, '', NULL);
+DELETE FROM `gameobject` WHERE `guid`=5530704;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES (5530704, 441101, 1, 0, 0, 1, 1, -2923.65, -239.7, 53.9153, 4.73752, -0, -0, -0.698165, 0.715937, 300, 0, 1, '', NULL);
+DELETE FROM `gameobject` WHERE `guid`=5530705;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES (5530705, 441101, 530, 0, 0, 1, 1, 10355.7, -6353.75, 32.6743, 5.22416, -0, -0, -0.505114, 0.863053, 300, 0, 1, '', NULL);
+DELETE FROM `gameobject` WHERE `guid`=5530706;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `ScriptName`, `VerifiedBuild`) VALUES (5530706, 441101, 609, 0, 0, 1, 1, 2372.47, -5674.58, 426.082, 2.4104, -0, -0, -0.933911, -0.357505, 300, 0, 1, '', NULL);
