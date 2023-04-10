27307f838765922e84dbf75cbd2a7bcfd0d980c5
AnchyDev <lewienato@gmail.com>
2023-04-10 21:18:01 +1000

    Cleanup of old code.

--- src/HardModeHandler.h
+++ src/HardModeHandler.h
@@ -1,10 +1,8 @@
 #ifndef MODULE_HARDMODE_HANDLER_H
 #define MODULE_HARDMODE_HANDLER_H
 
-#include "DifficultyMode.h"
-#include "HardModeReward.h"
+#include "HardModeTypes.h"
 
-#include "Player.h"
 #include <vector>
 
 class HardModeHandler
@@ -13,41 +11,20 @@ private:
     HardModeHandler() { }
 
 public:
-    void SetTainted(Player* player, bool value);
-    bool IsTainted(Player* player);
-    void SetShadowBanned(Player* player, bool value);
-    bool IsShadowBanned(Player* player);
-
-    std::string GetColorFromMode(uint8 mode);
-    std::string GetNameFromMode(uint8 mode);
-    std::string GetNamesFromEnabledModes(Player* player, bool colored = false);
-    void SendAlert(Player* player, std::string message);
-    bool HasModesEnabled(Player* player);
-    bool IsModeEnabledForPlayer(Player* player, uint8 mode);
-    bool IsModeEnabledForPlayerAndServer(Player* player, uint8 mode);
-    void UpdateAllModeEffects(Player* player);
-    bool TestForCrossplay(Player* target, Player* player);
-    uint32 GetEnabledModesAsMask(Player* player);
-    std::string GetConfigNameFromMode(uint8 mode);
-    PlayerSettingMap* GetPlayerSettingsFromDatabase(ObjectGuid guid);
-    std::string CreateWebhookObject(std::string title, std::string content);
-    void SendWebhookMessage(std::string payload);
-
-    void LoadRewardsFromDatabase();
-    std::vector<HardModeReward> GetRewardsForMode(uint8 mode);
-    void RewardPlayerForMode(Player* player, uint8 mode);
-    void RewardItems(Player* player, std::vector<HardModeReward> rewards);
-    void RewardTitle(Player* player, uint32 titleId);
-    void RewardSpell(Player* player, uint32 spellId);
-    void SendMailItems(Player* player, std::vector<std::pair<uint32, uint32>>& mailItems, std::string header, std::string body);
-
-    static HardModeHandler* GetInstance();
-
-public:
-    DifficultyMode* Modes[DifficultyModes::DIFFICULTY_MODE_COUNT];
+    void LoadHardModes();
+    void ClearHardModes();
+    std::vector<HardModeInfo>* GetHardModes();
 
 private:
-    std::vector<HardModeReward> _hardModeRewards;
+    std::vector<HardModeInfo> _hardModes;
+
+public:
+    static HardModeHandler* GetInstance()
+    {
+        static HardModeHandler instance;
+
+        return &instance;
+    }
 };
 
 #define sHardModeHandler HardModeHandler::GetInstance()
---
+++ src/HardModeTypes.h
@@ -0,0 +1,22 @@
+#ifndef MODULE_HARDMODE_TYPES_H
+#define MODULE_HARDMODE_TYPES_H
+
+#include "Common.h"
+
+struct HardModeInfo
+{
+    uint32 Id;
+    std::string Name;
+    std::string Description;
+    uint64 Restrictions;
+    bool Enabled;
+};
+
+enum HardModeRestrictions
+{
+    HARDMODE_RESTRICT_NONE = 0,
+    HARDMODE_RESTRICT_SELFCRAFT = 1, // The player can only use self-crafted items.
+    HARDMODE_RESTRICT_COUNT = 2, // The restriction count, used internally. DO NOT USE
+};
+
+#endif // MODULE_HARDMODE_TYPES_H
