fab6b41491ac2a4803efb259129f7ecd90215faf
AnchyDev <lewienato@gmail.com>
2023-04-11 10:08:43 +1000

    Replace old shrine object scripts with new one.

--- data/sql/db-world/updates/hardmode_world_2023_04_11_01.sql
+++ data/sql/db-world/updates/hardmode_world_2023_04_11_01.sql
@@ -7,3 +7,5 @@ CREATE TABLE IF NOT EXISTS `hardmode_modes` (
   `enabled` tinyint DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+UPDATE gameobject_template SET ScriptName = "HardModeShrineObject" WHERE entry = 441101;
