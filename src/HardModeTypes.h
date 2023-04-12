3ddaec83d786255df065d5f0497b44d4a1cc01ab
AnchyDev <lewienato@gmail.com>
2023-04-12 10:47:51 +1000

    Added future restrictions.

--- src/HardModeTypes.h
+++ src/HardModeTypes.h
@@ -19,12 +19,15 @@ struct HardModeInfo
 
 enum HardModeRestrictions
 {
-    HARDMODE_RESTRICT_NONE = 0, // Used internally, DO NOT USE
-    HARDMODE_RESTRICT_RETAIL_XP = 1, // 2: The player can only receive 1x exp rates from kills and quests.
-    HARDMODE_RESTRICT_SELFCRAFTED = 2, // 4: The player can only use self-crafted items (armor, weapons, consumables, etc..)
-    HARDMODE_RESTRICT_INTERACT_AUCTION = 3,// 8: The player cannot interact with the auction house.
-    HARDMODE_RESTRICT_INTERACT_GUILDBANK = 4, // 16: The player cannot interact with the guild bank.
-    HARDMODE_RESTRICT_COUNT = 5, // Used internally. DO NOT USE
+    HARDMODE_RESTRICT_NONE =                  0, // Used internally, DO NOT USE
+    HARDMODE_RESTRICT_RETAIL_XP =             1, // 2: The player can only receive 1x exp rates from kills and quests.
+    HARDMODE_RESTRICT_SELFCRAFTED =           2, // 4: The player can only use self-crafted items (armor, weapons, consumables, etc..)
+    HARDMODE_RESTRICT_INTERACT_AUCTION =      3, // 8: The player cannot interact with the auction house.
+    HARDMODE_RESTRICT_INTERACT_GUILDBANK =    4, // 16: The player cannot interact with the guild bank.
+    HARDMODE_RESTRICT_INTERACT_MAIL_SEND =    5, // 32: The player cannot send mail.
+    HARDMODE_RESTRICT_INTERACT_MAIL_RECEIVE = 6, // 64: The player cannot receive mail.
+    HARDMODE_RESTRICT_PERMADEATH =            7, // 128: The player is sent to the shadow tomb on death.
+    HARDMODE_RESTRICT_COUNT =                 8, // Used internally. DO NOT USE
 };
 
 enum HardModeRewardType
