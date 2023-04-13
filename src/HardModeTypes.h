5f1c229484afc0a08237134f41be9eb3f4c86145
AnchyDev <lewienato@gmail.com>
2023-04-13 15:33:06 +1000

    Made comment for verbose for actual functionality.

--- src/HardModeTypes.h
+++ src/HardModeTypes.h
@@ -23,7 +23,7 @@ struct HardModeInfo
 enum HardModeRestrictions
 {
     HARDMODE_RESTRICT_NONE =                  0, // Used internally, DO NOT USE
-    HARDMODE_RESTRICT_RETAIL_XP =             1, // 2: The player can only receive 1x exp rates from kills and quests.
+    HARDMODE_RESTRICT_RETAIL_XP =             1, // 2: The player can only receive 1x exp rates from kills, quests, etc..
     HARDMODE_RESTRICT_SELFCRAFTED =           2, // 4: The player can only use self-crafted items (armor, weapons, consumables, etc..)
     HARDMODE_RESTRICT_INTERACT_AUCTION =      3, // 8: The player cannot interact with the auction house.
     HARDMODE_RESTRICT_INTERACT_GUILDBANK =    4, // 16: The player cannot interact with the guild bank.
