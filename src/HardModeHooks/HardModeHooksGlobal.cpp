31861c769cffaa7a9f88a0919346d85cdc535e4a
AnchyDev <lewienato@gmail.com>
2023-04-28 16:13:25 +1000

    Added config for bad luck restriction.

--- src/HardModeHooks/HardModeHooksGlobal.cpp
+++ src/HardModeHooks/HardModeHooksGlobal.cpp
@@ -1,6 +1,7 @@
 #include "HardModeHooks/HardModeHooksGlobal.h"
 #include "HardModeHandler.h"
 
+#include "Config.h"
 #include "LootMgr.h"
 
 void HardModeHooksGlobalScript::OnBeforeDropAddItem(Player const* player, Loot& /*loot*/, bool /*canRate*/, uint16 /*lootMode*/, LootStoreItem* lootStoreItem, LootStore const& /*store*/)
@@ -34,9 +35,10 @@ void HardModeHooksGlobalScript::OnBeforeDropAddItem(Player const* player, Loot& 
         }
 
         auto roll = urand(0, 100);
-        if (roll > 10) // 10 percent chance to drop.
+        uint32 chance = sConfigMgr->GetOption<uint32>("HardMode.Restrict.BadLuck.Chance", 10);
+        if (roll <= chance)
         {
-            // If the roll is higher than 10 percent then it is deleted.
+            // If the roll is higher than x percent then it is deleted.
             lootStoreItem->itemid = 0;
         }
     }
